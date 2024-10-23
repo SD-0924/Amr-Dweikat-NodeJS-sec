6f6e35b808723c4604a3e433362d8b820ca86834
Amr-Dweikat939 <amrodwaikat@gmail.com>
2024-10-23 12:24:30 +0300

    update files

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("view engine", "ejs");
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Define a GET route to list all files
+// Define a GET route to list all files in the "data" directory
 app.get("/", (req, res) => {
   // Get all file names inside the data directory
   const fileNames = getFileNames();
@@ -31,29 +31,80 @@ app.get("/", (req, res) => {
   res.render("index", { files: fileNames });
 });
 
-// Validate file name before render page
-app.use("/files/:filename", (req, res, next) => {
-  const filename = req.params.filename;
-  if (!filename) {
-    return res
-      .status(400)
-      .send("Error: you should provide filename inside URL.");
-  }
+// Middleware to check if file exist or not
+const fileExists = (req, res, next) => {
   const files = getFileNames();
-  if (files.indexOf(filename) == -1) {
-    return res
-      .status(404)
-      .send(
-        'Error: either the file name is incorrect, or the file does not exist in the "data" directory, please check again.'
-      );
+  if (files.indexOf(req.params.filename) == -1) {
+    return res.status(404).json({
+      message: "Invalid file",
+      details:
+        'Either the file name is incorrect, or the file does not exist in the "data" directory, please check again',
+    });
   }
   next();
+};
+
+// Middleware to check whether the body data is in JSON format
+const validateBodyData = (req, res, next) => {
+  if (!req.body) {
+    return res.status(400).json({
+      message: "Invalid body data",
+      details: "The request body should be in JSON format",
+    });
+  }
+  next();
+};
+
+// Middleware to check whether the body data contains a file name
+const validateFileNameInBody = (req, res, next) => {
+  if (!req.body.hasOwnProperty("name")) {
+    return res.status(400).json({
+      message: "Invalid body data",
+      details:
+        "The request body should contain a 'name' property, which represents the file name you want to create",
+    });
+  } else if (!req.body.name) {
+    return res.status(400).json({
+      message: "Invalid file name",
+      details:
+        "You should provide 'name' property, which represents the file name you want to create",
+    });
+  }
+};
+const doesFileExist = (req, res, next) => {};
+const validateFileContentInBody = (req, res, next) => {};
+
+// Define a POST route to create a new file with a specified name and content
+app.post(
+  "/",
+  [
+    express.json(),
+    validateBodyData,
+    validateFileNameInBody,
+    doesFileExist,
+    validateFileContentInBody,
+  ],
+  (req, res) => {
+    const data = fs.writeFileSync(`./data/${req.body.name}`, req.body.content);
+    res
+      .status(201)
+      .json({ message: "Sucess", details: "File created successfully" });
+  }
+);
+
+// Define a GET route to view the content of a specific file
+app.get("/files/:filename", [fileExists], (req, res) => {
+  const data = fs.readFileSync(`./data/${req.params.filename}`, "utf-8");
+  res.render("detail", { fileContent: data });
 });
 
-// Define a GET route to render a specific file
-app.get("/files/:filename", (req, res) => {
-  const data = fs.readFileSync(`./data/${req.params.filename}`, "utf-8");
-  res.render("detail", { fileName: req.params.filename, fileContent: data });
+// Middleware to handle invalid routes
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Invalid route",
+    details:
+      "Please use one of the available routes: /, /create, or /files/filename",
+  });
 });
 
 // Make the server start listening
